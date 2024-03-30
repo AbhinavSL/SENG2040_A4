ac0a7ec159963f4823d2811afdc437064819fbcb
Abhinav <abhinavsl536@gmail.com>
2024-03-30 18:45:44 -0400

    Learning about diiferent ways for Ajax calls

--- index.js
+++ index.js
@@ -22,7 +22,36 @@ document.addEventListener('scroll', ()=>{
     console.log(postitionY)
 })
 
+const url = 'https://swapi.dev/api/people/' 
 
+//1. query ajax method:
 
+$.ajax({
+    type: 'GET',
+    url: url,
+    success: function(response){
+        console.log('jquery ajax', response)
+    },
+    error: function(error){
+        console.log(error)
+    }
+})
 
+//2. XMLHttpRequest:
 
+const req = new XMLHttpRequest()
+
+req.addEventListener('readystatechange', ()=>{
+    if(req.readyState===4){
+        console.log('xhttp', JSON.parse(req.responseText))
+    }
+})
+
+req.open('GET', url)
+req.send()
+
+//3. Fetch Method:
+
+fetch(url)
+.then(resp=> resp.json()).then(data=> console. log('fetch', data))
+.catch(err=> console. log(err))
