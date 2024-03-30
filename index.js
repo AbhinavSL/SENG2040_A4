f74f41c0bb7ee5a2018d1a6d814e97d7696e18c9
Abhinav <abhinavsl536@gmail.com>
2024-03-30 18:29:22 -0400

    Working on Event Listeners

--- index.js
+++ index.js
@@ -3,3 +3,26 @@ console.log('hello world')
 const testEL = document.getElementById('test-el')
 
 testEL.textContent = 'bye bye'
+
+testEL.addEventListener('click', ()=>{
+    console.log('clicked')
+    testEL.innerHTML = "<b>clicked</b>"
+})
+
+testEL.addEventListener('mouseover',()=>{
+    console.log('on')
+})
+
+testEL.addEventListener('mouseout',()=>{
+    console.log('off')
+})
+
+document.addEventListener('scroll', ()=>{
+    const postitionY = window.scrollY
+    console.log(postitionY)
+})
+
+
+
+
+
